2bce1e756b3d9188811f2c331a62feed92005cc8
Tam Dao <11000026+tamminhdao@users.noreply.github.com>
2018-03-05 15:57:44 -0600

    find the winning combos

--- lib/rules.ex
+++ lib/rules.ex
@@ -1,3 +1,26 @@
 defmodule Rules do
-  
+  def winning_combos(board) do
+    Enum.into(row(board), column(board))
+    |> Enum.into(diagonal(board))
+  end
+
+  def win?(board) do
+    if board == Board.empty_board() do
+      false
+    end
+  end
+
+  def row(board) do
+    Enum.chunk_every(board,3)
+  end
+
+  def column(board) do
+    [i0, i1, i2, i3, i4, i5, i6, i7, i8] = board
+    [[i0, i3, i6], [i1, i4, i7], [i2, i5, i8]]
+  end
+
+  def diagonal(board) do
+    [i0, _, i2, _, i4, _, i6, _, i8] = board
+    [[i0, i4, i8], [i2, i4, i6]]
+  end
 end
--- test/rules_test.exs
+++ test/rules_test.exs
@@ -1,4 +1,45 @@
 defmodule RulesTest do
   use ExUnit.Case
 
+    test "there is no win if the board is completely empty" do
+      empty_board = Board.empty_board()
+      assert Rules.win?(empty_board) == false
+    end
+
+    test "obtain all rows" do
+      board = [0,1,2,3,4,5,6,7,8]
+      assert Rules.row(board) == [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+    end
+
+    test "obtain all columns" do
+      board = [0,1,2,3,4,5,6,7,8]
+      assert Rules.column(board) == [[0, 3, 6], [1, 4, 7], [2, 5, 8]]
+    end
+
+    test "obtain all diagonals" do
+      board = [0,1,2,3,4,5,6,7,8]
+      assert Rules.diagonal(board) == [[0, 4, 8], [2, 4, 6]]
+    end
+
+    # test "winning by row" do
+    #   winning_board = Board.empty_board()
+    #   |> Board.insert_symbol(0, :player_one)
+    #   |> Board.insert_symbol(1, :player_one)
+    #   |> Board.insert_symbol(2, :player_one)
+    #   assert Rules.win?(winning_board) == true
+    # end
+
+    test "winning combos" do
+      board = [0,1,2,3,4,5,6,7,8]
+      assert Rules.winning_combos(board) == [
+                                              [0, 4, 8],
+                                              [2, 4, 6],
+                                              [0, 3, 6],
+                                              [1, 4, 7],
+                                              [2, 5, 8],
+                                              [0, 1, 2],
+                                              [3, 4, 5],
+                                              [6, 7, 8]
+                                            ]
+    end
 end
