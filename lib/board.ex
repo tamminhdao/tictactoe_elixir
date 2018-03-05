959c7a6f872416d674f68a8bedba598488916911
Tam Dao <11000026+tamminhdao@users.noreply.github.com>
2018-03-05 10:32:34 -0600

    replace nil call with :empty cell

--- lib/board.ex
+++ lib/board.ex
@@ -1,14 +1,10 @@
 defmodule Board do
   def empty_board(dimension \\ 3) do
     Enum.to_list 0..(dimension*dimension - 1)
-    |> Enum.map(fn(_) -> nil end)
+    |> Enum.map(fn(_) -> :empty end)
   end
 
   def insert_symbol(board, index, symbol) do
     List.replace_at(board, index, symbol)
   end
-
-  # def empty_cells(board) do
-  #   Enum.find_index(board, fn(x) -> x == nil end)
-  # end
 end
