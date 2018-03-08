696205b4b15be08752225f9993930456bf06f395
Tam Dao <11000026+tamminhdao@users.noreply.github.com>
2018-03-08 14:47:40 -0600

    cell_selection(_) needs an arg to match AI's implementation

--- lib/players/human_player.ex
+++ lib/players/human_player.ex
@@ -1,6 +1,6 @@
 defmodule HumanPlayer do
 
-  def cell_selection() do
+  def cell_selection(_) do
     cell = IO.gets "please enter your cell selection\n"
     cell |> String.trim |> String.to_integer
   end
--- test/players/human_player_test.exs
+++ test/players/human_player_test.exs
@@ -4,7 +4,7 @@ defmodule HumanPlayerTest do
 
   test "cell_selection() returns the correct integer from player's input" do
     capture_io([input: "1\n"], fn ->
-      assert HumanPlayer.cell_selection() == 1
+      assert HumanPlayer.cell_selection(:place_holder) == 1
     end)
   end
 end
